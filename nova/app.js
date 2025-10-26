05a923af90c199625146ec2e2bc97d2fa320875a
kaiser-ivranoch <kaiseratlas69@gmail.com>
2025-10-25 22:18:23 -0500

    error de orden arreglado

--- nova/app.js
+++ nova/app.js
@@ -1,17 +1,12 @@
-require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const socket = require('socket.io')
+require('dotenv').config()
 require('./database/connection')
 
 const app = express()
-const inventoryRouter = require('./routers/inventoryRouter')
-app.use('/inventory', inventoryRouter)
-
-const saleRouter = require('./routers/saleRouter')
-app.use('/sales', saleRouter)
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -19,23 +14,28 @@ app.set('view engine', 'ejs')
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cookieParser())
-
+app.use(cookieParser()) 
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.render('index') 
+  res.render('index')
 })
 
 const authRouter = require('./routers/authRouter')
-const dashboardRouter = require('./routers/dashboardRouter')
-
 app.use('/auth', authRouter)
-app.use('/dashboard', dashboardRouter)
+
+const verifyToken = require('./middlewares/authMiddleware')
+const dashboardRouter = require('./routers/dashboardRouter')
+const inventoryRouter = require('./routers/inventoryRouter')
+const saleRouter = require('./routers/saleRouter')
+
+app.use('/dashboard', verifyToken, dashboardRouter)
+app.use('/inventory', verifyToken, inventoryRouter)
+app.use('/sales', verifyToken, saleRouter)
 
 const server = require('http').createServer(app)
 const io = socket(server)
-require('./socket')(io) 
+require('./socket')(io)
 
 const PORT = process.env.PORT || 3000
 server.listen(PORT, () => {
